c82293aed50eaafc217c84033a1e86455cf47895
LucasLennon <lucasespir@gmail.com>
2019-10-22 12:47:46 -0300

    Add Material-Ui and Styled-Components to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import styled from "styled-components"
+import { AppBar } from "@material-ui/core"
+
 function App() {
   return (
     <div className="App">
