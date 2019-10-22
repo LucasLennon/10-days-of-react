ec9eec72cdc7e004951f6db887c456e6a1ce1cb2
LucasLennon <lucasespir@gmail.com>
2019-10-22 19:19:18 -0300

    Adding initial style for login page using Material-Ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 import styled from "styled-components"
-import { AppBar } from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles';
+import { 
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Container, 
+  Box,
+  Button
+} from "@material-ui/core"
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
+  const [form, setForm] = useState({
+    login: null,
+    password: null,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" className={classes.title}>
+            Login
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Box p={2}>
+          teste
+        </Box>
+      </Container>
+    </Box>
+
   );
 }
 
