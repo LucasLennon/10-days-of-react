241876b539ed163cdc6c2d0abc96fc7f8451cf19
LucasLennon <lucasespir@gmail.com>
2019-10-24 17:02:27 -0300

    Add Theme setup to project

--- src/core/layout/Layout.js
+++ src/core/layout/Layout.js
@@ -3,8 +3,6 @@ import React, {
   useState, 
   Fragment 
 } from 'react';
-import Login from "../page/Login";
-import Sidebar from "./BaseSidebar";
 
 import {
   BrowserRouter as Router,
@@ -12,15 +10,33 @@ import {
   Route,
 } from "react-router-dom";
 
-import { 
+import {
   AppBar,
   Toolbar,
   Typography,
   Box,
   IconButton
 } from "@material-ui/core"
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
+
+import Sidebar from "./BaseSidebar";
+
+import NewsList from "../page/NewsList";
+import Login from "../page/Login";
+
+const theme = createMuiTheme(
+  {
+    palette: {
+      primary: {
+        main: '#ff6600'
+      },
+      secondary: {
+        main: '#f6f6ef'
+      },
+    },
+  }
+)
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -43,39 +59,51 @@ function MainLayout() {
 
   return (
     <Router>
-      <Box>
-        <Sidebar open={sidebar} onClose={() => setSidebar(!sidebar)}/>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => setSidebar(!sidebar)}>
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              {pageName}
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Switch>
-          <Route exact path="/" render={
-            () => {
-              setPageName("Login")
-              return(
-                <Login />
-              )
-            }
-          } />
-          <Route path="/register" render={
-            () => {
-              setPageName("Register")
-              return (
-                <Fragment>
-                  Being created!
-                </Fragment>
-              )
-            }
-          } />
-        </Switch>
-      </Box>
+      <ThemeProvider theme={theme}>
+
+        <Box>
+          <Sidebar open={sidebar} onClose={() => setSidebar(!sidebar)}/>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => setSidebar(!sidebar)}>
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                {pageName}
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Switch>
+            <Route exact path="/" render={
+              () => {
+                setPageName("News")
+                return(
+                  <NewsList />
+                )
+              }
+            } />
+            <Route path="/Login" render={
+              () => {
+                setPageName("Login")
+                return(
+                  <Login />
+                )
+              }
+            } />
+            <Route path="/register" render={
+              () => {
+                setPageName("Register")
+                return (
+                  <Fragment>
+                    Being created!
+                  </Fragment>
+                )
+              }
+            } />
+          </Switch>
+        </Box>
+
+      </ThemeProvider>
     </Router>
   )
 }
